03dfe5b7ca51f3d1c27d9355efaa4204b6f613e9
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 16:29:49 -0400

    Migration for rooms and exits generated.

--- app/models.py
+++ app/models.py
@@ -3,13 +3,14 @@ import uuid
 # Create your models here.
 
 class Rooms(models.Model):
-    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)
+    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
     room_id = models.IntegerField()
     title = models.TextField(max_length=40)
     description = models.TextField(max_length=300)
     coordinates = models.CharField(max_length=40)
 
 class Exits(models.Model):
-    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
     direction = models.TextField()
     direction_id = models.IntegerField()
