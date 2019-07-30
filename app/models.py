1f264915ed37c3a75abfa0d68b884d7ebc966db1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 16:49:29 -0400

    Integer field rather than direction allowed to be blank on an Exits entry.

--- app/models.py
+++ app/models.py
@@ -2,14 +2,12 @@ from django.db import models
 # Create your models here.
 
 class Rooms(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False)
     room_id = models.IntegerField()
     title = models.TextField(max_length=40)
     description = models.TextField(max_length=300)
     coordinates = models.CharField(max_length=40)
 
 class Exits(models.Model):
-    id = models.IntegerField(primary_key=True, editable=False)
     room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
-    direction = models.TextField(blank=True)
-    direction_id = models.IntegerField()
+    direction = models.TextField()
+    direction_id = models.IntegerField(blank=True)
