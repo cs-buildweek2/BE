702cc2a793d07af9f433f8ceacd3deedf66720cb
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 15:56:33 -0400

    Added title and description. Added app to settings.py

--- app/models.py
+++ app/models.py
@@ -5,9 +5,11 @@ import uuid
 class Rooms(models.Model):
     id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)
     room_id = models.IntegerField()
+    title = models.TextField(max_length=40)
+    description = models.TextField(max_length=300)
     coordinates = models.CharField(max_length=40)
 
 class Exits(models.Model):
-    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
     direction = models.TextField()
     direction_id = models.IntegerField()
