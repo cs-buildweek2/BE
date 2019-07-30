9bc3c19e821848227a8ddf5e71dbb55b94234f0a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 16:43:22 -0400

    Allowed direction_id to be blank in exits. Changed Primary Key of each table to a regular integer.

--- app/models.py
+++ app/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-import uuid
 # Create your models here.
 
 class Rooms(models.Model):
-    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+    id = models.IntegerField(primary_key=True, editable=False)
     room_id = models.IntegerField()
     title = models.TextField(max_length=40)
     description = models.TextField(max_length=300)
     coordinates = models.CharField(max_length=40)
 
 class Exits(models.Model):
-    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=False, max_length=200)
+    id = models.IntegerField(primary_key=True, editable=False)
     room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
-    direction = models.TextField()
+    direction = models.TextField(blank=True)
     direction_id = models.IntegerField()
