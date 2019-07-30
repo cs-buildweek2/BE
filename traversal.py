136139a06836323d2dfda3c4815a059dbeb5a847
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 17:55:17 -0400

    Added initialization function, which will check the current room."

--- traversal.py
+++ traversal.py
@@ -1,17 +1,29 @@
 from app.models import Rooms, Exits
 import requests
-
-# curl -X POST -H 'Authorization: Token 7a375b52bdc410eebbc878ed3e58b2e94a8cb607' -H "Content-Type: application/json" -d '{"direction":"n"}' https://lambda-treasure-hunt.herokuapp.com/api/adv/move/
+import time
 
 def roomRequest(direction):
     secret = config('USER_KEY')
     URL = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/'
-    headers = {
-        'Authorization': f'Token {secret}'
+    headerOptions = {
+        'Authorization': f'Token {secret}',
+        'Content-Type': 'application/json'
     }
-    data = {
+    request = {
         "direction": direction
     }
+    response = requests.post(url = URL, headers = headerOptions, json = request)
+    return response.json()
+
+def initialization():
+    secret = config('USER_KEY')
+    URL = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
+    headerOptions = {
+        'Authorization': f'Token {secret}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(url = URL, headers = headerOptions)
+    return response.json()
 
 def traverseMap():
-    #
+    starting_room = initialization()
