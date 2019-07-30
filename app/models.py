79cdf4f2b78853ebb9909bd33eb8fb9792980655
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-30 13:02:40 -0400

    Migration models for Rooms and Exits tables created.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Rooms(models.Model):
+    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)
+    room_id = models.IntegerField()
+    coordinates = models.CharField(max_length=40)
+
+class Exits(models.Model):
+    id = models.UUIDFIELD(primary_key=True, default=uuid.uuid4 editable=False)room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    direction = models.TextField()
+    direction_id = models.IntegerField()
