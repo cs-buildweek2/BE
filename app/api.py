6e7138c3d31d1732f4480f91834cc98a73856a5a
Estevan <estevan.rodriguez10@yahoo.com>
2019-07-30 14:22:39 -0700

    started boilerplatting api.py to eventually start coding endpoints

--- app/api.py
+++ app/api.py
@@ -1,1 +1,12 @@
 # This file connects model to REST framework
+from rest_framework import serializers, viewsets
+from .models import Rooms
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model  = Rooms #tells us which model we are using
+        fields = ('room_id','coordinates') #tells us which fields from that model we want to use
+
+class RoomsViewSet(viewsets.ModelViewSet): #tells us rows were interested in showing
+    serializer_class = RoomSerializer #connects this class with RoomSerializer class
+    queryset = Rooms.objects.all() #tells us which things we want to return
